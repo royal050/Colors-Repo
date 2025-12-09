c43d32ef799684da3cfd4c728f427375e63ce536
andalosya <zensama999999@gmail.com>
2025-12-09 14:25:11 +0200

    Created The ColorsProject File

---
+++ ColorsSimpleProject.cs
@@ -0,0 +1,75 @@
+﻿namespace Colors
+{
+    static class Colors
+    {
+        public static void BlackColor()
+        {
+            Console.WriteLine("================================");
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        public static void DarkBlueColor()
+        {
+            Console.WriteLine("================================");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+        }
+        public static void DarkCyanColor()
+        {
+            Console.WriteLine("================================");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        }
+        public static void DarkGrayColor()
+        {
+            Console.WriteLine("================================");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+        }
+        public static void DarkGreenColor()
+        {
+            Console.WriteLine("================================");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+        public static void DarkMagentaColor()
+        {
+            Console.WriteLine("================================");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        }
+        public static void WhiteColor()
+        {
+            Console.WriteLine("================================");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+
+
+
+        public static void BlackColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        public static void DarkBlueColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+        }
+        public static void DarkCyanColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        }
+        public static void DarkGrayColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+        }
+        public static void DarkGreenColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+        public static void DarkMagentaColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        }
+        public static void WhiteColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+}
