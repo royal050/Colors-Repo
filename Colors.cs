2d35cfc7641dea71ae3ed6d0114e6bc065b137ab
Alimamdouh <zensama999999@gmail.com>
2025-12-15 08:58:49 +0200

    Add Colors.cs

---
+++ Colors.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Colors
+    {
+        public static void WhiteColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void GreenColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        public static void DarkGreenColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+        public static void RedColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        public static void DarkRedColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+        }
+
+        public static void BlueColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        public static void DarkBlueColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+        }
+
+        public static void YellowColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+        public static void DarkYellowColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+        }
+        public static void GrayColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        public static void DarkGrayColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+        }
+        public static void CyanColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+        public static void DarkCyanColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        }
+        public static void MagentaColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+        }
+        public static void DarkMagentaColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        }
+        public static void BlackColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        public static void ResetColor()
+        {
+            Console.WriteLine("================================");
+
+            Console.ResetColor();
+        }
+
+
+
+
+
+
+        public static void WhiteColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void GreenColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        public static void DarkGreenWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+        public static void RedColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        public static void DarkRedColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+        }
+        public static void BlueColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        public static void DarkBlueColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+        }
+        public static void YellowColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+        public static void DarkYellowColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+        }
+        public static void GrayColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        public static void DarkGrayColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+        }
+        public static void CyanColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+        public static void DarkCyanColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        }
+        public static void MagentaColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+        }
+        public static void DarkMagentaWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        }
+        public static void BlackColorWithOutSpace()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        public static void ResetColorWithOutSpace()
+        {
+            Console.ResetColor();
+        }
+    }
+}
