dcf043f1c664b4a8924c25b8d13061cb69833ba3
andalosya <aliroyal050@gmail.com>
2025-12-15 09:37:13 +0200

    modified The Colors Project

--- Colors.cs
+++ Colors.cs
@@ -123,69 +123,86 @@ namespace ConsoleApp1
         {
             Console.ForegroundColor = ConsoleColor.White;
         }
+
         public static void GreenColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.Green;
         }
+
         public static void DarkGreenWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.DarkGreen;
         }
+
         public static void RedColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.Red;
         }
+
         public static void DarkRedColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
         }
+
         public static void BlueColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.Blue;
         }
+
         public static void DarkBlueColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.DarkBlue;
         }
+
         public static void YellowColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
         }
+
         public static void DarkYellowColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.DarkYellow;
         }
+
         public static void GrayColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.Gray;
         }
+
         public static void DarkGrayColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.DarkGray;
         }
+
         public static void CyanColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
         }
+
         public static void DarkCyanColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.DarkCyan;
         }
+
         public static void MagentaColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.Magenta;
         }
+
         public static void DarkMagentaWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
         }
+
         public static void BlackColorWithOutSpace()
         {
             Console.ForegroundColor = ConsoleColor.Black;
         }
+
         public static void ResetColorWithOutSpace()
         {
             Console.ResetColor();
         }
+
     }
 }
